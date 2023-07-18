bf2a2c71609f03aba5003feb54984f95f0e0e610
igor <igor@flarehub.io>
2023-07-18 20:20:15 +0300

    first commit

---
+++ wordpress-block-css-test.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name:       Wordpress Block Css Test
+ * Description:       Example block scaffolded with Create Block tool.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wordpress-block-css-test
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_wordpress_block_css_test_block_init() {
+
+	register_block_type( __DIR__ . '/build', array(
+		'version' => "1.1.0",
+		'render_callback' => fn ($attributes) =>
+			'<p ' . get_block_wrapper_attributes() . '>' . $attributes['style']['color']['text'] . '</p>',
+		'attributes' => array(
+			'style' => array(
+				'type' => 'object',
+				'default' => array(
+					'color' => array( 'text' => '#ff0000')
+				),
+			),
+		),
+		'supports' => array(
+			'color' => array(
+				'text' => true,
+			),
+		),
+	)	);
+}
+add_action( 'init', 'create_block_wordpress_block_css_test_block_init' );
